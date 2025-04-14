16bcef855772205c5f2beb72e5e134fd7f011048
Lithemba <matthewkiwalabye@gmail.com>
2025-04-14 09:50:20 +0200

    added initial files

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { Route } from "react-router-dom";
+import Home from "./component/Home";
+import Products from "./components/Products";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Route path="/home">
+        <Home />
+      </Route>
+      <Route path="/products">
+        <Products />
+      </Route>
     </div>
   );
-}
+};
 
 export default App;
+
+// ourdomain.com/ => home screen or home component
+// ourdomain.com/products => product list component
---
+++ src/components/Products.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Products = () => {
+  return (
+    <div>Product list</div>
+  )
+}
+
+export default Products
